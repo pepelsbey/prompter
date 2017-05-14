d3099dc295767754aedd548d70adc70dc8a46f51
Vadim Makeev <pepelsbey@gmail.com>
2017-05-15 00:31:18 +0300

    Меняет табы на пробелы

--- script.js
+++ script.js
@@ -1,76 +1,76 @@
 (function() {
 
-	var prompt = document.querySelector('.prompt');
+    var prompt = document.querySelector('.prompt');
 
-	var animationLoop;
-	var scrollingNow = false;
-	var scrollStep = 3;
+    var animationLoop;
+    var scrollingNow = false;
+    var scrollStep = 3;
 
-	function scroll() {
-		if (window.scrollY) {
-			window.scrollBy(0, -scrollStep);
-			animationLoop = window.requestAnimationFrame(scroll);
-			scrollingNow = true;
-		}
-	}
+    function scroll() {
+        if (window.scrollY) {
+            window.scrollBy(0, -scrollStep);
+            animationLoop = window.requestAnimationFrame(scroll);
+            scrollingNow = true;
+        }
+    }
 
-	function pause() {
-		window.cancelAnimationFrame(animationLoop);
-		scrollingNow = false;
-	}
+    function pause() {
+        window.cancelAnimationFrame(animationLoop);
+        scrollingNow = false;
+    }
 
-	function toggle() {
-		if (scrollingNow) {
-			pause();
-		} else {
-			scroll();
-		}
-	}
+    function toggle() {
+        if (scrollingNow) {
+            pause();
+        } else {
+            scroll();
+        }
+    }
 
-	function start() {
-		window.scrollTo(0, document.body.clientHeight - window.innerHeight);
-	}
+    function start() {
+        window.scrollTo(0, document.body.clientHeight - window.innerHeight);
+    }
 
-	function resize(factor) {
-		prompt.style.setProperty(
-			'--size',
-			parseFloat(
-				prompt.style.getPropertyValue('--size')
-			) + factor
-		);
-	}
+    function resize(factor) {
+        prompt.style.setProperty(
+            '--size',
+            parseFloat(
+                prompt.style.getPropertyValue('--size')
+            ) + factor
+        );
+    }
 
-	document.addEventListener('keydown', function(event) {
-		if (event.metaKey || event.ctrlKey) {
-			switch (event.key) {
-				case '-':
-					event.preventDefault();
-					resize(-1);
-				break;
-				case '=':
-					event.preventDefault();
-					resize(+1);
-				break;
-			}
-		} else {
-			switch (event.key) {
-				case ' ':
-					event.preventDefault();
-					toggle();
-				break;
-				case '-':
-					if (scrollStep > 1) {
-						scrollStep--;
-					}
-				break;
-				case '=':
-					scrollStep++;
-				break;
-			}
-		}
-	});
+    document.addEventListener('keydown', function(event) {
+        if (event.metaKey || event.ctrlKey) {
+            switch (event.key) {
+                case '-':
+                    event.preventDefault();
+                    resize(-1);
+                break;
+                case '=':
+                    event.preventDefault();
+                    resize(+1);
+                break;
+            }
+        } else {
+            switch (event.key) {
+                case ' ':
+                    event.preventDefault();
+                    toggle();
+                break;
+                case '-':
+                    if (scrollStep > 1) {
+                        scrollStep--;
+                    }
+                break;
+                case '=':
+                    scrollStep++;
+                break;
+            }
+        }
+    });
 
-	document.addEventListener('click', toggle);
-	document.addEventListener('touchstart', toggle);
+    document.addEventListener('click', toggle);
+    document.addEventListener('touchstart', toggle);
 
 }());
